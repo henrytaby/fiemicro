e3ee9c291f90813c69ad0c79720deb91a8cc5c88
Henry Taby <henry.taby@gmail.com>
2023-01-12 18:53:03 -0400

    archivo basura

--- html/backend/logica_application/logs/log-2023-01-12.php
+++
@@ -1,9 +0,0 @@
-<?php  if ( ! defined('BASEPATH')) exit('No direct script access allowed'); ?>
-
-ERROR - 2023-01-12 17:19:14 --> Severity: Warning  --> fopen(/var/llavero/initium_key.txt): failed to open stream: No such file or directory /var/www/html/backend/logica_application/config/database.php 62
-ERROR - 2023-01-12 17:19:14 --> Cifrado conexion DB: No fue posible abrir el archivo llave.
-ERROR - 2023-01-12 17:23:14 --> Severity: Warning  --> mysqli_connect(): (HY000/2002): No such file or directory /var/www/html/backend/logica_system/database/drivers/mysqli/mysqli_driver.php 72
-ERROR - 2023-01-12 17:23:14 --> Unable to connect to the database
-ERROR - 2023-01-12 17:23:18 --> Severity: Warning  --> mysqli_connect(): (HY000/2002): No such file or directory /var/www/html/backend/logica_system/database/drivers/mysqli/mysqli_driver.php 72
-ERROR - 2023-01-12 17:23:18 --> Unable to connect to the database
-ERROR - 2023-01-12 18:35:57 --> Severity: Warning  --> A non-numeric value encountered /var/www/html/backend/logica_system/helpers/captcha_helper.php 96
